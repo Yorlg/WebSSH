91a8c5755da526881b905947ff4277ee97b384f6
LamGC <lam827@lamgc.net>
2023-04-24 11:13:14 +0000

    调整项目目录结构, 使用 express 重写 app.js;

--- server/app.js
+++ server/app.js
@@ -1,128 +1,126 @@
-const http = require('http');
-const WebSocket = require('ws');
+const express = require("express");
+const server = express();
+require("express-ws")(server);
+
+server.use(express.static("static/"));
+server.use(express.static("node_modules/"));
+
 const readline = require('readline');
 const { NodeSSH } = require('node-ssh');
 
 const ssh = new NodeSSH();
-const wss = new WebSocket.Server({ noServer: true });
 
-function handleConnection (socket) {
-  console.log('连接已建立');
+async function handleConnection(socket) {
+    console.log('连接已建立');
 
-  let isAlive = true;
-  let timer = null;
-  let sshCredentials;
-  let sshCredentialsReceived = false; // 新增状态变量
+    let isAlive = true;
+    let timer = null;
+    let sshCredentials;
+    let sshCredentialsReceived = false; // 新增状态变量
 
-  // 设置最大监听器数量
-  socket.setMaxListeners(15);
+    // 设置最大监听器数量
+    socket.setMaxListeners(15);
 
-  // 检查客户端是否活跃
-  const checkAlive = () => {
-    if (!isAlive) {
-      console.log('WebSocket 连接已超时');
-      socket.terminate();
-      return;
-    }
-
-    isAlive = false;
-    socket.ping(null, { mask: false, binary: true });
-
-    timer = setTimeout(checkAlive, 1000 * 60 * 10); // 10 分钟
-  };
-
-  socket.on('message', (data) => {
-
-    // 如果尚未接收到 SSH 凭据，则尝试解析 JSON
-    if (!sshCredentialsReceived) {
-      try {
-        sshCredentials = JSON.parse(data);
-        sshCredentialsReceived = true; // 更新状态变量
-      } catch (err) {
-        console.error('解析 SSH 凭据时出错', err);
-        socket.terminate();
-        return;
-      }
-
-      // 使用解析后的 SSH 凭据连接 SSH 服务器
-      ssh.connect({
-        host: sshCredentials.ip,
-        port: sshCredentials.port,
-        username: sshCredentials.username,
-        password: sshCredentials.password,
-      })
-        .then(() => {
-          console.log('SSH 连接已建立');
-
-          // 创建 SSH Shell
-          ssh.requestShell().then((stream) => {
-            // 使用 readline 处理终端输入
-            const rl = readline.createInterface({
-              input: process.stdin,
-              output: process.stdout,
-            });
-
-            rl.on('line', (line) => {
-              stream.write(line);
-            });
-
-            // 处理 SSH Shell 输入和输出
-            stream.on('data', (data) => {
-              isAlive = true;
-              socket.send(data.toString());
-            });
-
-            socket.on('message', (data) => {
-              if (sshCredentialsReceived) { // 确保 SSH 凭据已接收
-                isAlive = true;
-                stream.write(data);
-              }
-            });
-
-            socket.on('close', () => {
-              console.log('WebSocket 连接已关闭');
-              isAlive = true;
-              ssh.dispose(); // 关闭 SSH 连接
-              socket.terminate();
-            });
-
-            // 开始心跳检测
-            timer = setTimeout(checkAlive, 1000 * 60 * 10); // 10 分钟
-          }).catch((err) => {
-            console.error('创建 SSH Shell 失败', err);
+    // 检查客户端是否活跃
+    const checkAlive = () => {
+        if (!isAlive) {
+            console.log('WebSocket 连接已超时');
             socket.terminate();
-            ssh.dispose(); // 关闭 SSH 连接
-          });
-        }).catch((err) => {
-          console.error('SSH 连接错误', err);
-          socket.terminate();
-        });
-    } else {
-      // 处理非 SSH 凭据的数据（终端输入）
-      if (sshCredentialsReceived) {
-        isAlive = true;
-      }
-    }
-  });
+            return;
+        }
 
-  // 监听 WebSocket 连接错误
-  socket.on('error', (err) => {
-    console.error('WebSocket 连接错误', err);
-    ssh.dispose(); // 关闭 SSH 连接
-  });
+        isAlive = false;
+        socket.ping(null, { mask: false, binary: true });
+
+        timer = setTimeout(checkAlive, 1000 * 60 * 10); // 10 分钟
+    };
+
+    socket.on('message', (data) => {
+        // 如果尚未接收到 SSH 凭据，则尝试解析 JSON
+        if (!sshCredentialsReceived) {
+            try {
+                sshCredentials = JSON.parse(data);
+                sshCredentialsReceived = true; // 更新状态变量
+            } catch (err) {
+                console.error('解析 SSH 凭据时出错', err);
+                socket.terminate();
+                return;
+            }
+            
+            // 使用解析后的 SSH 凭据连接 SSH 服务器
+            ssh.connect({
+                host: sshCredentials.ip,
+                port: sshCredentials.port,
+                username: sshCredentials.username,
+                password: sshCredentials.password,
+            })
+                .then(() => {
+                    console.log('SSH 连接已建立');
+
+                    // 创建 SSH Shell
+                    ssh.requestShell().then((stream) => {
+                        // 使用 readline 处理终端输入
+                        const rl = readline.createInterface({
+                            input: process.stdin,
+                            output: process.stdout,
+                        });
+
+                        rl.on('line', (line) => {
+                            stream.write(line);
+                        });
+
+                        // 处理 SSH Shell 输入和输出
+                        stream.on('data', (data) => {
+                            isAlive = true;
+                            socket.send(data.toString());
+                        });
+
+                        socket.on('message', (data) => {
+                            if (sshCredentialsReceived) { // 确保 SSH 凭据已接收
+                                isAlive = true;
+                                stream.write(data);
+                            }
+                        });
+
+                        socket.on('close', () => {
+                            console.log('WebSocket 连接已关闭');
+                            isAlive = true;
+                            ssh.dispose(); // 关闭 SSH 连接
+                            socket.terminate();
+                        });
+
+                        // 开始心跳检测
+                        timer = setTimeout(checkAlive, 1000 * 60 * 10); // 10 分钟
+                    }).catch((err) => {
+                        console.error('创建 SSH Shell 失败', err);
+                        socket.terminate();
+                        ssh.dispose(); // 关闭 SSH 连接
+                    });
+                }).catch((err) => {
+                    console.error('SSH 连接错误', err);
+                    socket.terminate();
+                });
+        } else {
+            // 处理非 SSH 凭据的数据（终端输入）
+            if (sshCredentialsReceived) {
+                isAlive = true;
+            }
+        }
+    });
+
+    // 监听 WebSocket 连接错误
+    socket.on('error', (err) => {
+        console.error('WebSocket 连接错误', err);
+        ssh.dispose(); // 关闭 SSH 连接
+    });
 }
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('WebSocket 代理服务器');
+server.ws("/ssh", (ws, req) => {
+    // 连接成功后发送 test success
+    console.info("WebSocket 连接已建立.");
+    handleConnection(ws);
 });
 
-
-server.on('upgrade', (request, socket, head) => {
-  console.log('WebSocket 连接已升级');
-  wss.handleUpgrade(request, socket, head, handleConnection);
-});
-
-server.listen(3000, () => {
-  console.log('服务器已启动');
+server.listen(8080, () => {
+    console.info("服务器已启动");
 });
